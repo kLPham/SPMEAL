2d1e3fb7cd5ca1464f903b250ca2366eecb62bdd
Kelly Pham <Kellylpham@gmail.com>
2018-04-18 00:20:52 -0500

    fix errors

--- Server/controllers/mainCtrl.js
+++ Server/controllers/mainCtrl.js
@@ -1,12 +1,14 @@
 module.exports = {
-    create: (req, res, next) => {
-      const db = req.app.get("db");
-      const {
-        meal_id,
-        meal_type,
-        meal_title,
-        image_url,
-        price,
-        quantity,
-        description
-      } = req.body;
+  create: (req, res, next) => {
+    const db = req.app.get('db');
+    const {
+      meal_id,
+      meal_type,
+      meal_title,
+      image_url,
+      price,
+      quantity,
+      description
+    } = req.body;
+  }
+};
