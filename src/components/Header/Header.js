8dac0182159baf35bb00d5f18cba13deaa3351bd
Kelly Pham <Kellylpham@gmail.com>
2018-05-10 02:47:48 -0500

    added pop up and header toggle

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -13,6 +13,7 @@ import storeIcon from './storeIcon.jpg';
 import Cart from '../Cart/Cart';
 // import SearchBar from './components/SearchBar/SearchBar';
 // import Login from './components/Login/Login';
+import ReactTooltip from 'react-tooltip';
 
 export default class Header extends Component {
   constructor(props) {
@@ -20,13 +21,27 @@ export default class Header extends Component {
 
     //SET INITIAL STATE HERE:
     this.state = {
-      value: 1
+      value: 1,
+      active: null
     };
   }
 
   //HANDLE ACTION HERE:
   handleDropdownMenuChange(event, index, value) {
     this.setState({ value });
+  }
+  handleToggleChange(position) {
+    if (this.state.active === position) {
+      this.setState({ active: null });
+    } else {
+      this.setState({ active: position });
+    }
+  }
+  handleColorChange(position) {
+    if (this.state.active === position) {
+      return 'green';
+    }
+    return '';
   }
 
   render() {
@@ -64,65 +79,93 @@ export default class Header extends Component {
     return (
       // <MuiThemeProvider>
       <div>
-        {/* MATERIAL-UI BELOW */}
-
         <div className="secondHeader" style={header}>
           <img alt="icon" src={storeIcon} style={iconsLogo} />
           <div className="list">
             <Link to="/" className="span">
-              <span>
+              <span
+                style={{ color: this.handleColorChange(0) }}
+                // { background: this.handleBackgroundColorChange(0) },
+                onClick={() => {
+                  this.handleToggleChange(0);
+                }}
+              >
                 <FaHome style={homeIconStyle} />
               </span>
             </Link>
-            <Link to="/HowItWorks" className="span">
-              <span>How It Works</span>
-            </Link>
 
+            <Link to="/HowItWorks" className="span">
+              <span
+                style={{ color: this.handleColorChange(1) }}
+                onClick={() => {
+                  this.handleToggleChange(1);
+                }}
+              >
+                How It Works
+              </span>
+            </Link>
             <Link to="/About" className="span">
-              <span>About Us</span>
+              <span
+                style={{ color: this.handleColorChange(2) }}
+                onClick={() => {
+                  this.handleToggleChange(2);
+                }}
+              >
+                About Us
+              </span>
             </Link>
             <Link to="/Contact" className="span">
-              <span>Contact</span>
+              <span
+                style={{ color: this.handleColorChange(3) }}
+                onClick={() => {
+                  this.handleToggleChange(3);
+                }}
+              >
+                Contact
+              </span>
             </Link>
             {/* MENU DROPDOWN BELOW */}
-
-            <DropDownMenu
-              value={this.state.value}
-              onChange={this.handleDropdownMenuChange}
-              primaryText="menu"
-            >
-              <FaCutlery style={cuteryStyle}> </FaCutlery>
-
-              <MenuItem value={1} label="Menu">
-                <h2 className="centerMeal">Our Menu</h2>
-              </MenuItem>
-
-              <a href="/Meals/FullMenu" className="menuStyle">
-                <MenuItem value={2} primaryText="Full Menu" label="MENU" />
-              </a>
-              <a href="/Meals/SignatureMeals" className="menuStyle">
-                {/* <FaCutlery /> */}
-                <MenuItem
-                  value={3}
-                  label="Spartan Signature Meals"
-                  primaryText="Spartan Signature Meals"
-                />
-              </a>
-              <a href="/Meals/ByLB" className="menuStyle">
-                <MenuItem
-                  value={4}
-                  label="By The Pounds"
-                  primaryText="by the lbs"
-                />
-              </a>
-              <a href="/Meals/CustomMeals" className="menuStyle">
-                <MenuItem
-                  value={5}
-                  label="Custom Meals"
-                  primaryText="Custom Meals"
-                />
-              </a>
-            </DropDownMenu>
+            <a data-tip="Click here to see our full menu!">
+              <DropDownMenu
+                value={this.state.value}
+                onChange={this.handleDropdownMenuChange}
+                primaryText="menu"
+                style={{ color: this.handleColorChange(4) }}
+                onClick={() => {
+                  this.handleToggleChange(4);
+                }}
+              >
+                <FaCutlery style={cuteryStyle} />
+                <MenuItem value={1} label="Menu">
+                  <h2>Our Menu</h2>
+                </MenuItem>
+                <a href="/Meals/FullMenu" className="menuStyle">
+                  <MenuItem value={2} primaryText="Full Menu" label="MENU" />
+                </a>
+                <a href="/Meals/SignatureMeals" className="menuStyle">
+                  <MenuItem
+                    value={3}
+                    label="Spartan Signature Meals"
+                    primaryText="Spartan Signature Meals"
+                  />
+                </a>
+                <a href="/Meals/ByLB" className="menuStyle">
+                  <MenuItem
+                    value={4}
+                    label="By The Pounds"
+                    primaryText="by the lbs"
+                  />
+                </a>
+                <a href="/Meals/CustomMeals" className="menuStyle">
+                  <MenuItem
+                    value={5}
+                    label="Custom Meals"
+                    primaryText="Custom Meals"
+                  />
+                </a>
+              </DropDownMenu>
+            </a>
+            <ReactTooltip place="bottom" type="success" effect="float" />
           </div>
           <Cart />
           <RaisedButton label="Default" />
