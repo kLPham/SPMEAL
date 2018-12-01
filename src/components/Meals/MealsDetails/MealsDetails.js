d556f1030462594148d86367edd7b3bb8cf8d257
Kelly Pham <Kellylpham@gmail.com>
2018-12-01 00:29:31 -0600

    charts

--- src/components/Meals/MealsDetails/MealsDetails.js
+++ src/components/Meals/MealsDetails/MealsDetails.js
@@ -12,6 +12,13 @@ import Cart from '../../Cart/Cart';
 // import FullSizeCartView from '../../Cart/FullSizeCartView';
 import Swal from 'sweetalert2';
 
+import microwave from './microwave.png';
+import mitten from './mitten.png';
+import '../Meals.css';
+
+//importing the all the charts here:
+import { LineChart, PieChart, BarChart } from 'react-chartkick';
+
 export default class MealsDetails extends Component {
   constructor(props) {
     super(props);
@@ -78,6 +85,19 @@ export default class MealsDetails extends Component {
     ];
 
     const displayMealDetails = this.state.mealsToDisplay.map(mealsId => {
+      // data={[["Blueberry", 44], ["Strawberry", 23], ...]}
+      //data here:
+      // const dataPie = {
+      //     [ 'Diary & Eggs',20], ['Produce',30],['Grain',5],['Sugar & Fat',10],['Meat',20],['Other',15]
+      //   };
+      const data = [
+        { name: 'Diary & Eggs', data: { '2017-01-01': 3, '2017-01-02': 4 } },
+        { name: 'Produce', data: { '2017-01-01': 5, '2017-01-02': 3 } },
+        { name: 'Grain', data: { '2017-01-01': 5, '2017-01-02': 3 } },
+        { name: 'Sugar & Fat', data: { '2017-01-01': 5, '2017-01-02': 3 } },
+        { name: 'Meat', data: { '2017-01-01': 5, '2017-01-02': 3 } },
+        { name: 'Other', data: { '2017-01-01': 5, '2017-01-02': 3 } }
+      ];
       return (
         <div key={mealsId.meals_id} style={{ display: 'flex' }}>
           {/* //LEft Side: */}
@@ -103,7 +123,108 @@ export default class MealsDetails extends Component {
             <p style={{ color: '#565353', fontSize: '16px' }}>
               {mealsId.description}
             </p>
-            <p>${mealsId.price}</p>
+            <div
+              style={{
+                display: 'flex',
+                paddingTop: '5%',
+                marginRight: '5%',
+                marginLeft: '5%',
+                width: '100%',
+                fontWeight: 700
+              }}
+            >
+              <p style={{ marginRight: '70%' }}>{mealsId.calories} cal</p>
+              <p
+                style={{
+                  borderStyle: 'ridge',
+                  borderWidth: '1px',
+                  borderColor: 'black',
+                  borderRadius: '10px',
+                  padding: '5px'
+                }}
+              >
+                ${mealsId.price}
+              </p>
+            </div>
+            <hr />
+            <div>
+              <h2
+                style={{ fontWeight: 900, fontSize: '25px', display: 'flex' }}
+              >
+                Ingredients
+              </h2>
+              <p style={{ fontWeight: 700, fontSize: '17px' }}>contains:</p>
+              <p>{mealsId.ingredients}</p>
+            </div>
+            <hr />
+            <div>
+              <h2
+                style={{ fontWeight: 900, fontSize: '25px', display: 'flex' }}
+              >
+                Nutrition profile
+              </h2>
+              <div style={{ display: 'flex', width: '100%' }}>
+                <PieChart
+                  data={[
+                    ['Diary & Eggs', 20],
+                    ['Produce', 30],
+                    ['Grain', 5],
+                    ['Sugar & Fat', 10],
+                    ['Meat', 20],
+                    ['Other', 15]
+                  ]}
+                />
+                {/* <LineChart data={data} /> */}
+                <BarChart
+                  data={[['protein', 46], ['carbs', 31], ['fat', 17]]}
+                />
+              </div>
+            </div>
+            <hr />
+            <div style={{ width: '80%' }}>
+              <h2
+                style={{ fontWeight: 900, fontSize: '25px', display: 'flex' }}
+              >
+                How to prepare
+              </h2>
+              <div
+                style={{
+                  display: 'flex',
+                  marginLeft: '20%',
+                  marginTop: '10%',
+                  fontSize: '17px',
+                  justifyContent: 'spaceAround'
+                }}
+              >
+                <p style={{ fontWeight: 700, marginRight: '30%' }}>
+                  <img
+                    alt="image of a mitten"
+                    src={mitten}
+                    style={{
+                      height: '50px',
+                      width: '50px',
+                      display: 'flex'
+                    }}
+                  />
+                  Oven
+                  <p style={{ fontWeight: 'lighter' }}>{mealsId.oven}</p>
+                </p>
+                <p style={{ marginLeft: '30%', fontWeight: 700 }}>
+                  <img
+                    alt="image of a microwave"
+                    src={microwave}
+                    style={{
+                      height: '50px',
+                      width: '50px',
+                      display: 'flex'
+                    }}
+                  />
+                  Microwave
+                  <p style={{ fontWeight: 'lighter' }}>{mealsId.microwave}</p>
+                </p>
+              </div>
+            </div>
+            <hr />
             <div style={{ display: 'flex' }}>
               <Button
                 color="black"
